5c8cbc9dfa4a0b451b8a2952dfffb28215ac80aa
Titus Muthomi <muthomititus+github@gmail.com>
2023-06-07 12:07:18 +0300

    Add generic error handler

--- src/main/java/com/titusm/springbootdemo/exceptions/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/titusm/springbootdemo/exceptions/RestResponseEntityExceptionHandler.java
@@ -15,4 +15,11 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
     public ErrorMessage employeeNotFoundHandler(EmployeeNotFoundException exception) {
         return new ErrorMessage(HttpStatus.NOT_FOUND, exception.getMessage());
     }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorMessage genericExceptionHandler(Exception exception) {
+        return new ErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
+    }
 }
